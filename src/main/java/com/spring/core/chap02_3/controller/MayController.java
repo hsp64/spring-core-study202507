a213674024af91f37a0f9e4b84da406f25731fda
hsp64 <pphs604@gmail.com>
2025-07-19 18:08:33 +0900

    서비스 계층 간 의존성 주입 구현: Repository를 Service에, Service를 Controller에 주입하여 데이터 정제 및 응답 처리로직 연결.

--- src/main/java/com/spring/core/chap02_3/controller/MayController.java
+++ src/main/java/com/spring/core/chap02_3/controller/MayController.java
@@ -1,12 +1,23 @@
 package com.spring.core.chap02_3.controller;
 
 
+import com.spring.core.chap02_3.service.MyService;
+
 // 클라이언트의 요청을 처리하는 클래스
 public class MayController {
 
+    private MyService service;
+
+    public MayController(MyService service) {
+        System.out.println("MyController객체 생성!");
+        this.service = service;
+        System.out.println("Service 의존성이 Controller에 주입됨!");
+    }
+
+    // 정제된 데이터를 응답해주세요!
     public void handleRequest() {
 
-        System.out.println("정제된 데이터를 응답 -> ");
+        System.out.println("정제된 데이터를 응답 -> "+ service.processData());
     }
 }
 
