1d834486dea41570cc63f799b2c97c16776399bd
hsp64 <pphs604@gmail.com>
2025-07-20 15:29:38 +0900

    학생 관리 및 상품 관련 스프링 구성 및 클래스 추가: - `StudentConfig` 추가하여 학생 관리 컴포넌트 스캔 및 구성. - `@Repository`, `@Controller`, `@Service`를 활용한 학생 및 상품 관리 클래스 개발:   - `StudentRepository`, `StudentMemoryRepository`, `StudentDatabaseRepository`, `StudentController` 추가.   - 제품 검색을 위한 `ProductService`와 `ProductRepository` 개발. - `Student` 모델 정의 및 Lombok 애노테이션 적용. - 학생 및 제품 관련 단위 테스트 클래스 작성 (`StudentControllerTest`, `ProductServiceTest`). - Spring Application 설정 추가: `application.properties` 업데이트.

---
+++ src/test/java/com/spring/core/chap02_3/service/ProductServiceTest.java
@@ -0,0 +1,20 @@
+package com.spring.core.chap02_3.service;
+
+import com.spring.core.SpringCore202507Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ProductServiceTest {
+
+    @Test
+    void test() {
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext(SpringCore202507Application.class);
+
+        ProductService service = context.getBean(ProductService.class);
+        String result = service.findProduct();
+        System.out.println("result = " + result);
+    }
+
+}
